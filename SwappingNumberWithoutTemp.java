f3490c8450dd9923959e59b3644841010222344f
Kumbharetanuja <144601758+Kumbharetanuja@users.noreply.github.com>
2026-05-31 23:37:42 +0530

    Add files via upload

---
+++ SwappingNumberWithoutTemp.java
@@ -0,0 +1,22 @@
+class SwappingNumberWithoutTemp
+{
+public static void main(String[] args)
+{
+int a=10;
+int b=20;
+
+System.out.println("After Swapping");
+System.out.println("A = " + a);
+System.out.println("B = " + b);
+
+
+a=a+b;
+b=a-b;
+a=a-b;
+
+System.out.println("Before Swapping");
+System.out.println("A = " + a);
+System.out.println("B = " + b);
+
+}
+}
