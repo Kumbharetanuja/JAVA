7b19f7e80a550e1b05b58da51a06f71152ac4218
Kumbharetanuja <144601758+Kumbharetanuja@users.noreply.github.com>
2026-06-03 23:54:52 +0530

    Add files via upload

---
+++ Percentage.java
@@ -0,0 +1,11 @@
+class Percentage{
+public static void main(String[] args)
+{
+ int totalOptainedMarks = 587;
+ int totalMarks=625;
+
+//logic
+double percentage = totalOptainedMarks * 100.0/totalMarks;
+System.out.println(percentage); //93.92
+}
+}
