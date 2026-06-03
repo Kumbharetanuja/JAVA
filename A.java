8fe6542a2d8fd80230b1db9c0227823b41e8d208
Kumbharetanuja <144601758+Kumbharetanuja@users.noreply.github.com>
2026-06-03 23:50:31 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,16 @@
+class A
+{
+public static void main(String[] args)
+{
+int a =12;
+System.out.println(a);
+{
+int b = 45;
+a = 34;
+System.out.println(a);
+System.out.println(b);
+}
+System.out.println(a);
+System.out.println(b);
+}
+}
