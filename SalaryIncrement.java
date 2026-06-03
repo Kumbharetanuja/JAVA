d8bafec08bc97575ae6703232f2592c2ffab078f
Kumbharetanuja <144601758+Kumbharetanuja@users.noreply.github.com>
2026-06-03 23:49:45 +0530

    Add files via upload

---
+++ SalaryIncrement.java
@@ -0,0 +1,17 @@
+// WAP Where Salary how be increment by 25% store this  and update it in same variable.
+
+class SalaryIncrement
+{
+public static void main (String[] args)
+{
+double salary = 6_50_000;
+System.out.println("Salary: ");
+System.out.println(salary);
+
+//logic for salary increment
+
+salary = salary * 1.25;
+System.out.println("After 25% Increment,Salary: ");
+System.out.println(salary);
+}
+}
