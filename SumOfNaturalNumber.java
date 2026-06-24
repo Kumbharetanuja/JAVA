8527927bf66a48c089fa645e62c292cd0dc7864f
Kumbharetanuja <144601758+Kumbharetanuja@users.noreply.github.com>
2026-06-24 22:57:26 +0530

    Add files via upload

---
+++ SumOfNaturalNumber.java
@@ -0,0 +1,14 @@
+class SumOfNaturalNumber{
+public static void main(String[] args)
+{
+int n = 10;
+int sum = 0;
+int i = 1;
+while (i <= n)
+{
+ sum = sum + i;
+ i++;
+}
+System.out.println("Sum of 1st " +n+ " natural number is: " + sum);
+}
+}
