09775ff32c81a5be496abe80a277ff59c1811c02
Kumbharetanuja <144601758+Kumbharetanuja@users.noreply.github.com>
2026-06-24 22:55:31 +0530

    Add files via upload

---
+++ UnderstandingWhile.java
@@ -0,0 +1,12 @@
+class UnderstandingWhile
+{
+public static void main(String[] args)
+{
+int i=1; //Intialization
+while(i<=5) // Condition
+{
+System.out.println("Hello A18 student ");
+i++; //Updation
+}
+}
+}
