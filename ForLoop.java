2d05ab4c7684e67d96b499e301ac39e12bb8f51f
Kumbharetanuja <144601758+Kumbharetanuja@users.noreply.github.com>
2026-06-29 21:53:36 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,12 @@
+class ForLoop
+{
+public static void main(String[] args)
+{
+ int i=1;
+ for(System.out.print("Salam "); i<=5 ;System.out.println("bhai"))
+ {
+   System.out.print("Rocky ");
+     i++;
+ }
+}
+}
