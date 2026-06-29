fd4fc06a33bb6e9067180e3914e373d3f1751d7a
Kumbharetanuja <144601758+Kumbharetanuja@users.noreply.github.com>
2026-06-29 21:50:37 +0530

    Add files via upload

---
+++ CountDigit.java
@@ -0,0 +1,16 @@
+class CountDigit
+{
+public static void main(String[] args)
+{
+  int num = 0;
+  int count = 0;
+  int temp = num;
+  do
+  {
+    count++;
+    num = num / 10;
+  }
+  while(num != 0);
+  System.out.println("The number of digit in " + temp+ " is: " + count);
+}
+}
